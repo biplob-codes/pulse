eaef738c2a6b1f75eacb50e6c0e27cea05c5d580
Biplob Talukdar <biplob.codes@gmail.com>
2026-05-10 01:56:46 +0600

    Show voters list

---
+++ app/[slug]/(public)/[board]/p/[feedback]/VotersList.tsx
@@ -0,0 +1,38 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+interface Props {
+  voters: {
+    id: string;
+    name: string;
+    image: string | null;
+  }[];
+}
+
+export function VotersList({ voters }: Props) {
+  return (
+    <div className=" border border-border  rounded-sm p-5 w-9/10">
+      <p className=" text-gray-500 text-sm font-semibold  mb-3">VOTERS</p>
+
+      <div className="flex flex-col gap-2.5">
+        {voters.map((voter) => (
+          <div key={voter.id} className="flex items-center gap-2.5">
+            <Avatar className="w-7 h-7 text-xs">
+              <AvatarImage src={voter.image ?? undefined} />
+              <AvatarFallback className="bg-muted text-muted-foreground">
+                {voter.name
+                  .split(" ")
+                  .map((n) => n[0])
+                  .join("")
+                  .slice(0, 2)
+                  .toUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+            <span className="text-[15px] text-gray-800 dark:text-gray-200">
+              {voter.name}
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- app/[slug]/(public)/[board]/p/[feedback]/page.tsx
+++ app/[slug]/(public)/[board]/p/[feedback]/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
-
-const FeedbackDetailsPage = () => {
-  return (
-    <div>FeedbackDetailsPage</div>
-  )
+import React from "react";
+import { VotersList } from "./VotersList";
+import prisma from "@/lib/prisma";
+interface Props {
+  params: Promise<{
+    feedback: string;
+  }>;
 }
+const FeedbackDetailsPage = async ({ params }: Props) => {
+  const { feedback: slug } = await params;
+  const feedback = await prisma.feedback.findUnique({
+    where: {
+      slug,
+    },
+    include: {
+      votes: { include: { user: true } },
+    },
+  });
+  if (!feedback) {
+    return <div>Feedback not found</div>;
+  }
+  const voters = feedback?.votes.map((v) => v.user);
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3">
+      <div className="  px-3 py-4">
+        <div className="sticky top-25">
+          <VotersList voters={voters} />
+        </div>
+      </div>
+      <div className="col-span-2 px-6 py-4">details</div>
+    </div>
+  );
+};
 
-export default FeedbackDetailsPage
+export default FeedbackDetailsPage;
